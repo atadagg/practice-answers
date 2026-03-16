44c17036ef0df5adbe39585f623752cf7f8bbfa0
atadagg <98890853+atadagg@users.noreply.github.com>
2026-03-16 11:34:02 +0300

    Add optimized solution for longest substring problem

--- solutions/3. Longest Substring Without Repeating Characters.cpp
+++ solutions/3. Longest Substring Without Repeating Characters.cpp
@@ -18,3 +18,29 @@ public:
         return size;
     }
 };
+
+//max speed version with a 128 slot array:
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> last_seen(128,-1);
+
+        int left = 0;
+        int max_size = 0;
+
+
+        for (int right = 0; right < s.size(); right++){
+            char current_char = s[right];
+
+            if(last_seen[current_char] >= left){
+                left = last_seen[current_char] + 1;
+            }
+
+            last_seen[current_char] = right;
+
+            max_size = max(max_size, right - left + 1);
+        }
+        return max_size;
+    }
+};
